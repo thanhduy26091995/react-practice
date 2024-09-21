238a44d46a6aa633f08135097d1bca23a9a01809
DESKTOP-L482ENJ\Admin <buivuthanhduy@gmail.com>
2024-09-21 23:39:54 +0700

    Chore: working with Ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -38,7 +38,7 @@ function App() {
   )
 }
 
-export default App
+export default Stopwatch
 
 const user = {
   name: 'Dennis Bui',
@@ -118,3 +118,41 @@ export function Profile() {
   )
 }
 
+function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  return (
+    <>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>
+        Start
+      </button>
+
+      <button onClick={handleStop}>
+        Stop
+      </button>
+    </>
+  );
+}
