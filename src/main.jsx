20cfac8bcc1ace108c23a85eed03dd75f4a8b64b
DESKTOP-L482ENJ\Admin <buivuthanhduy@gmail.com>
2024-09-21 17:26:10 +0700

    Chore: Basic done Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import Square from './TicTacToe.jsx'
+import Game from './TicTacToe.jsx'
+import App from './FilterableProduct.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Square />
+    <App />
   </StrictMode>,
 )
