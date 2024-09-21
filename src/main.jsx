53f9badb0e2a609d994c14d355d30545b1aecb74
DESKTOP-L482ENJ\Admin <buivuthanhduy@gmail.com>
2024-09-21 23:39:54 +0700

    Chore: working with Ref

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Game from './TicTacToe.jsx'
 import App from './FilterableProduct.jsx'
+import Stopwatch from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Stopwatch />
   </StrictMode>,
 )
