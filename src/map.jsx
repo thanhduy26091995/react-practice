2dcbe96dcd137d43fb331df8e91b03d457ddb28b
DESKTOP-L482ENJ\Admin <buivuthanhduy@gmail.com>
2024-09-23 00:15:40 +0700

    Chore: Use effect

---
+++ src/map.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+import { MapWidget } from "./map-widget";
+
+export default function Map({ zoomLevel }) {
+  const containerRef = useRef(null);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (mapRef.current == null) {
+      mapRef.current = new MapWidget(containerRef.current);
+    }
+
+    const map = mapRef.current;
+    map.setZoom(zoomLevel);
+  }, [zoomLevel]);
+
+  return <div style={{ width: 200, height: 200 }} ref={containerRef} />;
+}
