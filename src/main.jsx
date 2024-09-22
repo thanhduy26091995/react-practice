ff8e2ebd2eabe9fb1cb7fce9eaac83bdd14df5f9
DESKTOP-L482ENJ\Admin <buivuthanhduy@gmail.com>
2024-09-22 23:48:16 +0700

    Chore: Interacting with use effect

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import Game from './TicTacToe.jsx'
 import App from './FilterableProduct.jsx'
 import Stopwatch from './App.jsx'
+import UseEffectExampleApp from './use-effect-example.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Stopwatch />
+    <UseEffectExampleApp />
   </StrictMode>,
 )
